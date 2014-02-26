78882cb6bea09cabcf10cbf9fb6ee22c81d7e07a
Anz_MSI <pjc0247@naver.com>
2014-02-26 15:39:47 +0900

    DCLP singleton 클래스 추가

---
+++ src/ThreadSafeSingleton.h
@@ -0,0 +1,49 @@
+#ifndef _THREAD_SAFE_SINGLETON_H
+#define _THREAD_SAFE_SINGLETON_H
+
+#include <atomic>
+#include <mutex>
+
+template <typename T>
+class ThreadSafeSingleton{
+public:
+	ThreadSafeSingleton(void){
+	}
+	virtual ~ThreadSafeSingleton(void){
+	}
+
+	static T* getInstance(){
+		auto ptr = instance.load();
+
+		if( ptr == nullptr ){
+			std::lock_guard<std::mutex> guard(lock);
+
+			ptr = instance.load();
+			if( ptr == nullptr ){
+				ptr = new T();
+				instance.store( ptr );
+			}
+		}
+		return ptr;
+	}
+
+	static void releaseInstance(){
+		delete instance.load();
+		instance.store( nullptr );
+	}
+
+private:
+	static std::mutex lock;
+	static std::atomic<T*> instance;
+
+};
+
+template <typename T>
+std::atomic<T*> ThreadSafeSingleton<T>::instance = nullptr;
+
+template <typename T>
+std::mutex ThreadSafeSingleton<T>::lock;
+
+
+
+#endif //_THREAD_SAFE_SINGLETON_H
