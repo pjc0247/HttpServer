da1149f170718d5f05095ee1c597b41749b4121b
Anz_MSI <pjc0247@naver.com>
2014-03-07 16:00:45 +0900

    pch 수정

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,16 +1,7 @@
-// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
-// 표준 시스템 포함 파일 및 프로젝트 관련 포함 파일이
-// 들어 있는 포함 파일입니다.
-//
-
-#pragma once
-
-#include "targetver.h"
+#ifndef _STDAFX_H
+#define _STDAFX_H
 
 #include <stdio.h>
-#include <tchar.h>
-
 #include <assert.h>
 
-
-// TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
+#endif //_STDAFX_H
