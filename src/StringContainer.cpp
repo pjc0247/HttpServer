5c0d8d0d454a369abbbe933884b5c9c71a3246ec
pjc0247_msi <pjc0247@naver.com>
2014-03-12 17:30:40 +0900

    container 추가

---
+++ src/StringContainer.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+
+#include "StringContainer.h"
+
+using namespace std;
+
+StringContainer::StringContainer(){
+
+}
+
+bool StringContainer::load(const std::string &str){
+	int offset = 0;
+	int length = str.length();
+
+	string key, value;
+
+	for(int i=0;i<length;i++){
+		if( str[i] == '=' ){
+			key = str.substr( offset, i-offset );
+			offset = i+1;
+		}
+		else if( str[i] == ';' ){
+			value = str.substr( offset, i-offset );
+			offset = i+1;
+
+			set( key, value );
+		}
+	}
+
+	if( offset != length ){
+		value = str.substr( offset, length-offset );
+
+		set( key, value );
+	}
+
+	return true;
+}
